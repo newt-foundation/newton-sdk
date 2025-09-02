a42e49cfb40412f791f4388d2bb76b68abbf69cf
David He <david.he@magic.link>
2025-09-02 03:55:58 -0400

    put in a stub for wait for task created as well

--- src/modules/avs/index.ts
+++ src/modules/avs/index.ts
@@ -5,23 +5,89 @@ import { SubmitEvaluationParams, TaskCreated, TaskId, TaskResponded, TaskStatus 
 import { createJsonRpcRequestPayload } from '@core/utils/json-rpc';
 import { PublicClient } from 'viem';
 
+interface CreateTaskResult {
+  receipt: any;
+  task_request_id: Hex;
+  timestamp: number;
+}
+
+// this is ostensibly the task result query
+interface WaitForTaskIdResult {
+  task_request_id: string;
+  task_request: any;
+  status: 'Queued' | 'Processing' | 'Completed' | 'Failed';
+  result?: {
+    task_id?: Hex;
+    tx_hash?: Hex;
+  };
+  error?: unknown;
+  processing_time_ms?: number;
+}
+
 interface PendingTaskBuilder {
-  getTaskId: () => Promise<TaskId>;
-  waitForTaskCreated: () => Promise<TaskCreated>;
+  getTaskId: () => Promise<WaitForTaskIdResult>;
+  waitForTaskCreated: () => Promise<WaitForTaskIdResult>;
   waitForTaskResponded: () => Promise<TaskResponded>;
 }
 
-const waitForTaskCreated = (
+const getTaskId = async (
   publicClient: PublicClient,
   args: {
-    taskId: TaskId;
+    taskRequestId: string;
     client?: PublicClient; // optionally specify WS-enabled client
     timeoutMs?: number; // default e.g., 30_000
     abortSignal?: AbortSignal;
   },
-): Promise<TaskCreated> => {
-  console.log('waitForTaskCreated args: ', args, publicClient);
-  throw new Error('Newton SDK: waitForTaskCreated Not implemented');
+): Promise<WaitForTaskIdResult> => {
+  const endpoint = publicClient.chain?.testnet ? TESTNET_AVS_API : MAINNET_AVS_API;
+
+  const body = createJsonRpcRequestPayload(AVS_METHODS.waitForTaskId, {
+    task_request_id: args.taskRequestId,
+    timeout_secs: args.timeoutMs ?? 60,
+  });
+
+  // Question: This endpoint blocks until the task is completed or the timeout is reached -> do you mean until the task is created?
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+  if (data.error) {
+    throw new Error(`Newton SDK: getTaskId failed: ${data.error.message}`);
+  }
+  // this assumes no need for a polling mechanism for now.
+  return data.result as WaitForTaskIdResult;
+};
+
+const waitForTaskCreated = async (
+  publicClient: PublicClient,
+  args: {
+    taskRequestId: string;
+    client?: PublicClient; // optionally specify WS-enabled client
+    timeoutMs?: number; // default e.g., 30_000
+    abortSignal?: AbortSignal;
+  },
+): Promise<WaitForTaskIdResult> => {
+  const endpoint = publicClient.chain?.testnet ? TESTNET_AVS_API : MAINNET_AVS_API;
+
+  const body = createJsonRpcRequestPayload(AVS_METHODS.waitForTaskId, {
+    task_request_id: args.taskRequestId,
+    timeout_secs: args.timeoutMs ?? 60,
+  });
+
+  // Question: This endpoint blocks until the task is completed or the timeout is reached -> do you mean until the task is created?
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+  if (data.error) {
+    throw new Error(`Newton SDK: getTaskId failed: ${data.error.message}`);
+  }
+  // this assumes no need for a polling mechanism for now.
+  return data.result as WaitForTaskIdResult;
 };
 const waitForTaskResponded = (
   publicClient: PublicClient,
@@ -60,9 +126,7 @@ const getTaskStatus = (publicClient: PublicClient, args: { taskId: TaskId }): Pr
 const submitEvaluationRequest = async (
   publicClient: PublicClient,
   args: SubmitEvaluationParams,
-): Promise<
-  ({ ok: true; taskRequestId: string; txHash?: Hex } & PendingTaskBuilder) | { ok: false; error: NewtonError }
-> => {
+): Promise<({ ok: true; taskRequestId: string } & PendingTaskBuilder) | { ok: false; error: NewtonError }> => {
   const endpoint = publicClient?.chain?.testnet ? TESTNET_AVS_API : MAINNET_AVS_API;
   const body = createJsonRpcRequestPayload(AVS_METHODS.createTask, args);
   const res = await fetch(endpoint, {
@@ -74,13 +138,13 @@ const submitEvaluationRequest = async (
   if (data.error) {
     return { ok: false, error: data.error };
   }
+  const createTaskResult = data.result as CreateTaskResult;
   return {
     ok: true,
-    taskRequestId: data.result.task_request_id,
-    txHash: data.result.txHash,
-    getTaskId: () => Promise.resolve(data.result.task_id),
-    waitForTaskCreated: () => Promise.resolve(data.result.task_id),
-    waitForTaskResponded: () => Promise.resolve(data.result.task_id),
+    taskRequestId: createTaskResult.task_request_id,
+    getTaskId: () => getTaskId(publicClient, { taskRequestId: createTaskResult.task_request_id }),
+    waitForTaskCreated: () => waitForTaskCreated(publicClient, { taskRequestId: createTaskResult.task_request_id }),
+    waitForTaskResponded: () => Promise.resolve({} as any),
   };
 };
 export {
