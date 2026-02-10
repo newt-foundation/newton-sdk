ee15e1c71da92c2089a7791b4414064444da4595
David He <david.he@magic.link>
2026-02-10 15:00:42 -0500

    add prod addresses for base sepolia

--- src/const.ts
+++ src/const.ts
@@ -19,11 +19,11 @@ export const GATEWAY_METHODS = {
 export const NEWTON_PROVER_TASK_MANAGER: Record<number, Hex> = {
   [sepolia.id]: '0xecb741f4875770f9a5f060cb30f6c9eb5966ed13',
   [mainnet.id]: '0x2010dbaa5438801bdc3f08174a799fe344f544ee',
-  [baseSepolia.id]: '0x', //TODO: replace with prod addresses
+  [baseSepolia.id]: '0xc3dc89b5e5241ef53c468dfbaa371a7f1d465eb7',
 };
 
 export const ATTESTATION_VALIDATOR: Record<number, Hex> = {
   [sepolia.id]: '0x26f452e4b9c9c28508cb836ba486cceaa95b429c',
   [mainnet.id]: '0x263c275c15867a4611a44c600e77144a23012a06',
-  [baseSepolia.id]: '0x', //TODO: replace with prod addresses
+  [baseSepolia.id]: '0x4e21b596944ebe8bd3773dd70c6f134bd2840a73',
 };
